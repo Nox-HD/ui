0a197751bd569c435a204777a32230f26ed15501
Dilukshan <dilukshann7@gmail.com>
2024-08-29 14:32:47 +0530

    Search Console Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    siteUrl: process.env.SITE_URL || 'https://ui.noxhd.com',
-    generateRobotsTxt: true,
     output: 'export',
     images: { unoptimized: true },
     trailingSlash: true
