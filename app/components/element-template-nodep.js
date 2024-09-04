f6f0ee6a653e9bb687242ae2fcdcd15ae11409d2
Dilukshan <dilukshann7@gmail.com>
2024-09-05 03:19:30 +0530

    Minor Bug Fix

--- app/components/element-template-nodep.js
+++ app/components/element-template-nodep.js
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from 'react';
 
 const TemplateX = ( { heading, subtitle, element, tabcode, dependencies, csscode, jsxcode, jsxlocation, csslocation } ) => {
+  const [copied1, setCopied1] = useState(false)
   const [Csscodes, setCsscode] = useState(true)
   const [Jsxcodes, setJsxcode] = useState(true)
 
@@ -22,6 +23,8 @@ const TemplateX = ( { heading, subtitle, element, tabcode, dependencies, csscode
 
     checkCodes();
   }, [csscode, jsxcode]);
+
+
 
   return (
     <div>
@@ -93,8 +96,9 @@ const TemplateX = ( { heading, subtitle, element, tabcode, dependencies, csscode
                                   }}>
                   {jsxcode}
                   </SyntaxHighlighter>
-                  <button className='p-2 absolute right-0 top-3 hover:bg-[#3f3f46] transition-colors duration-200 rounded-sm' onClick={async () => {navigator.clipboard.writeText(jsxcode)}}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clipboard h-3 w-3"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path></svg>
+                  <button className='p-2 absolute right-0 top-3 hover:bg-[#3f3f46] transition-colors duration-200 rounded-sm' onClick={async () => {navigator.clipboard.writeText(jsxcode), setCopied1(true),   setTimeout(() => {setCopied1(false)}, 1500)}}>
+                    {!copied1 && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clipboard h-3 w-3"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path></svg>}
+                    {copied1 && <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>}
                   </button>
                 </div> 
               </div> )}
