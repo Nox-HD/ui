0f5d57ef24489644556569ccca042dd3eb5e83d4
Dilukshan Niranjan <65407969+DilukshanN7@users.noreply.github.com>
2024-09-24 17:13:10 +0530

    OpenGraph Optimization

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,21 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export const metadata = {
   title: "NoxHD UI - UI Library for Next-Gen Websites",
   description: "UI Library for Next-Gen Websites",
+  openGraph: {
+    title: 'NoxHD UI',
+    description: 'Advanced UI Library for Modern Web Applications',
+    url: 'https://ui.noxhd.com',
+    siteName: 'NoxHD UI',
+    images: [
+      {
+        url: 'logo.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
