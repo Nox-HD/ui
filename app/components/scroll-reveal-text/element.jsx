5c135b79960a3d8cc9b28d1ea4a940ea4b2a089e
Dilukshan Niranjan <65407969+DilukshanN7@users.noreply.github.com>
2024-09-28 13:53:34 +0530

    New Component: Scroll Reveal Text

---
+++ app/components/scroll-reveal-text/element.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+const Element = () => {
+  return (
+    <div>
+      <div>
+          
+      </div>
+    </div>
+  )
+}
+
+export default Element
