0f5efc310f38ec5e80373490c159993e1071e4d9
Dilukshan Niranjan <65407969+DilukshanN7@users.noreply.github.com>
2024-11-07 17:48:56 +0530

    new component

---
+++ app/components/animated-nav-bar/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TemplateNoCentre from "../element-template-nocentre";
+
+const AwardsCarousel = () => {
+  return (
+    <div>
+      <TemplateNoCentre
+        heading={""}
+        subtitle={""}
+        element={""}
+        tabcode={``}
+        dependencies={""}
+        csscode={""}
+        jsxcode={``}
+        jsxlocation={""}
+        csslocation={""}
+      />
+    </div>
+  );
+};
+
+export default AwardsCarousel;
