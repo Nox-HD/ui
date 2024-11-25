d7ac992f70412e59cfde793274736c5725daa333
Dilukshan N <65407969+DilukshanN7@users.noreply.github.com>
2024-11-26 00:28:34 +0530

    final fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://ui.noxhd.com',
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: true,
     outDir: "./out"
-    // ...other options
   }
